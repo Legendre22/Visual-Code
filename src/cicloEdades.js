b372967ee428830d2274a437db71ef501a44f61a
Lucas Andrada <lukaazzz22@gmail.com>
2023-01-01 18:42:26 -0300

    Practice

---
+++ src/cicloEdades.js
@@ -0,0 +1,41 @@
+arrayEdades = [5,10,23,18];
+posicion = 0;
+posiciondo = 0;
+
+// ciclo for
+
+for (i='0'; i < 4; i++) {
+    if (arrayEdades[i] >=18);
+
+        console.log("Es mayor de edad");
+}
+    else {
+        console.log("No es mayor de edad");
+    }
+
+    //ciclo while
+
+
+while(posicion <=3)
+    if (arrayEdades[posicion] >= 18) {
+        console.log("Es mayor de edad");
+    } 
+    else {
+        console.log("No es mayor de edad");
+    }
+    posicion = posicion + 1;
+
+    //ciclo do-while
+
+    do {
+        if
+        (arrayEdades[posiciondo] >=18) {
+            console.log("Es mayor de edad");
+        }
+    }
+    else {
+            console.log("No es mayor de edad");
+    }
+    posiciondo = posiciondo + 1;
+
+    while (posiciondo >=3);
---
+++ src/script.js
@@ -0,0 +1,13 @@
+switch (edad) {
+    case edad < 6:
+        "Primera etapa. Primera infancia 0-5 años"
+    case edad > 5 <= 12:
+        "Segunda etapa. Infancia 6-12 años"
+    case edad > 12 <= 17:
+        "Tercera etapa. Adolescente 13-17 años"
+    case edad > 17 <= 26:
+        "Cuarta etapa. Adulto joven 18-26 años"
+    case edad > 26 <= 59:
+        "Quinta etapa. Adulto mayor 27-59 años"
+}
+   
